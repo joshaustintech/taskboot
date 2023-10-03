e7af47d2c79a6fb22e48721f8bd0c28a3ae89ada
Joshua Austin <83935880+joshaustintech@users.noreply.github.com>
2023-10-03 16:36:25 -0500

    initial commit

---
+++ src/test/java/tech/joshaustin/taskboot/TaskbootApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.joshaustin.taskboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
